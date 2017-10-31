3a87758d38e9e4dbf5c28b5ffaf566b40c1065b1
Claudio Cavalcante Tonhá <claudio.cth@gmail.com>
2017-10-30 23:44:13 -0200

    Integrando webservice do node.js
    Integração webservice node.js

--- app/controllers/api/v1/trilhas_controller.rb
+++ app/controllers/api/v1/trilhas_controller.rb
@@ -1,9 +1,22 @@
 module Api
     module V1
+        require 'net/http'
+        require 'json'
+        require 'uri'
         class TrilhasController < ApplicationController
             def index
                 trilhas = Trilha.order('created_at DESC');
                 render json: {status: 'SUCESSO!', message:'Trilhas mais próximas', data: trilhas}, status: :ok
+            end
+
+            def bikes
+                url = URI.parse("http://localhost:4004/api/bikes")
+                req = Net::HTTP::Get.new(url.path)
+                res = Net::HTTP.start(url.host, url.port){|http|
+                    http.request(req)
+                }
+                @tipoAtividades = JSON.parse(res.body)
+                render json: {status: 'SUCESSO!', message:'Bikes', data: @tipoAtividades}, status: :ok               
             end
         end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :trilhas, only: :index
+      get "bikes", to: "trilhas#bikes"
     end
   end
 end
